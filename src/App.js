a1974f16eb27645e265fdd917d5b0d220a050b22
fryingFreddo <frederik.roe@outlook.com>
2022-05-04 19:37:26 +0200

    clean up & react to unity functions added in App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import {
   Auth,
 } from "senf-atomic-design-system";
 import { ThemeProvider } from "styled-components";
+import { useEffect, useState } from "react";
 
 const unityContent = new UnityContent(
   "build/WebGLBuild.json",
@@ -29,6 +30,77 @@ const UnityWrapper = styled.div`
   z-index: 10;
 `;
 const App = () => {
+
+  const [objSelected, setIsObjSelected] = useState(true);
+  useEffect(function () {
+    unityContent.on("isObjActive", function (isActive) {
+      setIsObjSelected(isActive);
+      if (isActive) { console.log("Object depending on Context Menu is Active"); }
+    });
+  }, []);
+  /*//#region spawn objects
+  function spawnCube() {
+    unityContext.send("BuildingManager", "SpawnObject", 0);
+  }
+  function spawnSphere() {
+    unityContext.send("BuildingManager", "SpawnObject", 1);
+  }
+  function spawnCylinder() {
+    unityContext.send("BuildingManager", "SpawnObject", 2);
+  }
+  function spawnMarker() {
+    unityContext.send("BuildingManager", "SpawnObject", 3);
+  }
+  //#endregion
+  //#region marker toggle
+  function hideMarkers() {
+    unityContext.send("Canvas", "HideMarkers");
+  }
+  function ShowMarkers() {
+    unityContext.send("Canvas", "ShowMarkers");
+  }
+  //#endregion
+  //#region Switch Cams
+  function switchToTopView() {
+  
+    unityContext.send("Canvas", "SwitchToTopView");
+  }
+  
+  function switchToNormalView() {
+  
+    unityContext.send("Canvas", "SwitchToNormalView");
+  }
+  
+  function switchToStreetView() {
+  
+    unityContext.send("Canvas", "SwitchToStreetView");
+  }
+  //#endregion
+  //#region ObjectContextMenu
+  
+  function setTag(index) {
+    unityContext.send("BuildingManager", "setTag", index); // 0 = Wohnen; 1= Buero; 2 = Kultur
+  }
+  
+  function deleteObject() {
+    unityContext.send("BuildingManager", "DestroyObject");
+  }
+  
+  function scaleObject(newValue) {
+    unityContext.send("BuildingManager", "ScaleSliderUpdate", newValue);
+  }
+  
+  function rotateObject(newValue) {
+    unityContext.send("BuildingManager", "RotateSliderUpdate", newValue);
+  }
+  //#endregion
+  
+  //#region einreichen
+  function handInProposal() {
+    unityContext.send("Canvas", "TakeScreenshot");
+    unityContext.send("Canvas", "CreateText");
+  }
+  //#endregion */
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
